c387cb471e9a8a9975883d6a0d0616de54a6770e
Hal Gentz <zegentzy@protonmail.com>
2018-10-28 18:20:02 -0600

    Bucket
    Signed-off-by: Hal Gentz <zegentzy@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(drain_filter)]
 use rand::prelude::*;
 use rand::distributions::{Distribution, Standard};
 //use lazy_static::lazy_static;
@@ -6,12 +7,12 @@ use std::thread::LocalKey;
 use std::time::{Duration, Instant};
 use std::cmp::Ordering;
 
-type Time = u32;
+type Time = u16;
 const ENTITY_COUNT: usize = 1_000_000;
 //const ENTITY_COUNT: usize = 1_0;
-const CHUNK_SIZE: usize = ENTITY_COUNT * 4;
-const ROUNDS: usize = 10;
-const TIME_MARGIN: usize = (u8::max_value() as usize * CHUNK_SIZE * 2); // times 2 for saftey
+const CHUNK_SIZE: usize = ENTITY_COUNT * 10;
+const ROUNDS: usize = 4;
+const TIME_MARGIN: usize = (u8::max_value() as usize * CHUNK_SIZE / ENTITY_COUNT * 10); // times 10 for saftey
 const TIME_CAP: Time = Time::max_value() - TIME_MARGIN as Time;
 const PREGENED_RANDOM_U8: usize = CHUNK_SIZE * ROUNDS + ENTITY_COUNT + 10;
 const NUM_BUCKETS: usize = 256;
@@ -61,7 +62,7 @@ thread_local! {
     static U8RAND: PregenedRand<u8> = PregenedRand::new(PREGENED_RANDOM_U8);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Entity {
     time: Time,
 }
@@ -74,128 +75,42 @@ impl Entity {
     }
 }
 
-fn make_order(b: usize, bt: usize, e: &[Entity]) -> Vec<(usize, Time)> {
-    let mut ret = e
-        .iter()
-        .enumerate()
-        .filter_map(|(i, e)| if e.time as usize % bt == b { Some((i, e.time)) } else { None } )
-        .collect::<Vec<_>>();
-    ret.sort_unstable_by(|a, b| b.1.cmp(&a.1));
-    ret
-}
-
 fn do_turns(num_buckets: usize) -> Duration {
     PregenedRand::reset(&U8RAND);
+    println!("Rand Gen'd");
 
     let mut entities = vec![];
     for _ in 0..ENTITY_COUNT {
         entities.push(Entity::new());
     }
+    println!("Entities Gen'd");
 
-    let mut orders = vec![];
-    for i in 0..num_buckets {
-        orders.push(make_order(i, num_buckets, &entities));
-    }
-
-    let mut bucket_orders = vec![];
-    for (i, order) in (&orders).iter().enumerate() {
-        bucket_orders.push((i, order.last().map(|o| *o)));
-    }
-
-    type BucketOrderType = (usize, Option<(usize, Time)>);
-    fn bucket_orders_cmp(a: &BucketOrderType, b: &BucketOrderType) -> Ordering {
-        match (a.1, b.1) {
-            (None, None) => Ordering::Equal,
-            (None, Some(_)) => Ordering::Less,
-            (Some(_), None) => Ordering::Greater,
-            (Some(a), Some(b)) => b.1.cmp(&a.1),
-        }
-    }
-    bucket_orders.sort_unstable_by(bucket_orders_cmp);
+    fn sort_fn(a: &Time, b: &Time) -> Ordering { b.cmp(a) }
+    fn bucket_fn(a: &Time, num_buckets: usize) -> usize { *a as usize % num_buckets }
+    let mut bucket = Bucket::new(entities.iter().map(|e| e.time).collect(), sort_fn, bucket_fn, num_buckets);
+    println!("Buckets Gen'd");
 
     let now = Instant::now();
     println!("Started");
     for i in 0..ROUNDS {
         println!("Round {}", i);
         for _ in 0..CHUNK_SIZE {
-            //println!("bucket_orders {:?}", bucket_orders);
-
-            let cur_b = bucket_orders.pop().unwrap();
-            let cur = orders[cur_b.0].pop().unwrap();
-
-            //println!("Doing {:?}", cur);
-
-            let new_elem = (cur_b.0, orders[cur_b.0].last().map(|o| *o));
-            let pos = bucket_orders
-                .binary_search_by(|a| bucket_orders_cmp(a, &new_elem))
-                .unwrap_or_else(|o| o);
-            bucket_orders.insert(pos, new_elem);
-
-            entities[cur.0].time += PregenedRand::next(&U8RAND) as Time;
-
-            let new_elem = (cur.0, entities[cur.0].time);
-            let i = new_elem.1 as usize % num_buckets;
-
-            let pos = orders[i]
-                .binary_search_by(|a| new_elem.1.cmp(&a.1))
-                .unwrap_or_else(|o| o);
-
-            if pos == orders[i].len() {
-                let end = (i, orders[i].last().map(|o| *o));
-                let rpos = bucket_orders
-                    .binary_search_by(|a| bucket_orders_cmp(a, &end))
-                    .unwrap();
-
-                let mut frpos = rpos;
-                let mut found = false;
-                while frpos < bucket_orders.len() && bucket_orders_cmp(&bucket_orders[frpos], &end) == Ordering::Equal {
-                    if bucket_orders[frpos].0 == i {
-                        found = true;
-                        break;
-                    }
-                    frpos += 1;
-                }
-
-                if !found && frpos != 0 {
-                    frpos = rpos - 1;
-                    while bucket_orders_cmp(&bucket_orders[frpos], &end) == Ordering::Equal {
-                        if bucket_orders[frpos].0 == i {
-                            found = true;
-                            break;
-                        }
-                        if frpos == 0 {
-                            break;
-                        }
-                        frpos -= 1;
-                    }
-                }
-
-                assert!(found);
-                bucket_orders.remove(frpos);
-
-                let new_elem = (i, Some(new_elem));
-                let pos = bucket_orders
-                    .binary_search_by(|a| bucket_orders_cmp(a, &new_elem))
-                    .unwrap_or_else(|o| o);
-                assert!(pos >= frpos);
-                bucket_orders.insert(pos, new_elem);
-            }
-            orders[i].insert(pos, new_elem);
+            let (index, _item) = bucket.pop(sort_fn);
+            entities[index].time += PregenedRand::next(&U8RAND) as Time;
+            bucket.reinsert(index, entities[index].time, sort_fn, bucket_fn);
         }
 
-
-        println!("Late Round {}", i);
-        let max = entities.iter().max_by(|a, b| a.time.cmp(&b.time)).unwrap().time;
+        let max = bucket.max(sort_fn).unwrap();
+        println!("Late Round {} {}", i, max);
         if max > TIME_CAP {
-            println!("Rebase! {}", i);
-            let min = entities.iter().min_by(|a, b| a.time.cmp(&b.time)).unwrap().time;
+            let min = bucket.min().unwrap();
+            println!("Rebase! {} {}", i, min);
+
             entities.iter_mut().for_each(|e| e.time -= min);
+            bucket = bucket.modify(|e| *e -= min, sort_fn, bucket_fn);
+            println!("Buckets Gen'd");
 
-            for order in &mut orders {
-                order.iter_mut().for_each(|o| o.1 -= min);
-            }
-
-            let max = entities.iter().max_by(|a, b| a.time.cmp(&b.time)).unwrap().time;
+            let max = bucket.max(sort_fn).unwrap();
             if max > TIME_CAP {
                 panic!("Spread too large.");
             }
@@ -207,9 +122,187 @@ fn do_turns(num_buckets: usize) -> Duration {
 fn main() {
     println!("Margin {}, Cap {}, Max {}", TIME_MARGIN, TIME_CAP, Time::max_value());
     println!("{} Entities, {} Actions", ENTITY_COUNT, ROUNDS * CHUNK_SIZE);
-    assert!(TIME_MARGIN < TIME_CAP as usize);
+    assert!(TIME_MARGIN < Time::max_value() as usize);
 
-    for i in &[256] {
+    for i in &[NUM_BUCKETS] {
         println!("{} took {:?}", i, do_turns(*i));
+    }
+}
+
+// We got lots of memory overhead, but we are fast.
+struct Bucket<T> {
+    items: Vec<T>,
+    buckets: Vec<Vec<(usize, T)>>,
+    heads: Vec<(usize, Option<(usize, T)>)>,
+}
+
+impl<T> Bucket<T>
+where
+    T: std::clone::Clone,
+{
+    #[inline]
+    fn head_cmp<S>(
+        a: &Option<(usize, T)>,
+        b: &Option<(usize, T)>,
+        sort_fn: &mut S
+    ) -> Ordering
+    where
+        S: FnMut(&T, &T) -> Ordering,
+    {
+        match (&a, &b) {
+            (None, None) => Ordering::Equal,
+            (None, Some(_)) => Ordering::Less,
+            (Some(_), None) => Ordering::Greater,
+            (Some((_, ref a)), Some((_, ref b))) => sort_fn(a, b),
+        }
+    }
+
+    fn new<S, B>(items: Vec<T>, mut sort_fn: S, mut bucket_fn: B, num_buckets: usize) -> Self
+    where
+        S: FnMut(&T, &T) -> Ordering,
+        B: FnMut(&T, usize) -> usize,
+    {
+        let mut items_c = items
+            .clone()
+            .into_iter()
+            .enumerate()
+            .collect::<Vec<_>>();
+
+        let mut buckets = vec![];
+        buckets.reserve(num_buckets);
+        for i in 0..num_buckets {
+            let mut bucket = items_c
+                .drain_filter(|(_, t)| {
+                    let b = bucket_fn(t, num_buckets);
+                    assert!(b < num_buckets);
+                    b == i
+                })
+                .collect::<Vec<_>>();
+            bucket.sort_unstable_by(|(_, a), (_, b)| sort_fn(a, b));
+            buckets.push(bucket);
+        }
+
+        let mut heads = vec![];
+        heads.reserve(num_buckets);
+        for (i, bucket) in (&buckets).iter().enumerate() {
+            heads.push((i, bucket.last().map(|o| o.clone())));
+        }
+        heads.sort_unstable_by(|(_, a), (_, b)| Bucket::<T>::head_cmp(a, b, &mut sort_fn));
+
+        Bucket {
+            items,
+            buckets,
+            heads,
+        }
+    }
+
+    // Returns the index and the item
+    fn pop<S>(&mut self, mut sort_fn: S) -> (usize, T)
+    where
+        S: FnMut(&T, &T) -> Ordering,
+    {
+        let head = self.heads.pop().unwrap();
+        let cur = self.buckets[head.0].pop().unwrap();
+        assert_eq!(sort_fn(&cur.1, &head.1.unwrap().1), Ordering::Equal);
+
+        let new_head = (head.0, self.buckets[head.0].last().map(|o| o.clone()));
+        let pos = self.heads
+            .binary_search_by(|a| Bucket::<T>::head_cmp(&a.1, &new_head.1, &mut sort_fn))
+            .unwrap_or_else(|o| o);
+        self.heads.insert(pos, new_head);
+
+        cur
+    }
+
+    // Index must be the index recieved when item was poped.
+    fn reinsert<S, B>(&mut self, index: usize, item: T, mut sort_fn: S, mut bucket_fn: B)
+    where
+        S: FnMut(&T, &T) -> Ordering,
+        B: FnMut(&T, usize) -> usize,
+    {
+        let i = bucket_fn(&item, self.buckets.len());
+        assert!(i < self.buckets.len());
+
+        let pos = self.buckets[i]
+            .binary_search_by(|(_, a)| sort_fn(a, &item))
+            .unwrap_or_else(|o| o);
+        let new_elem = (index, item);
+
+        if pos == self.buckets[i].len() {
+            let end = (i, self.buckets[i].last().map(|o| o.clone()));
+            let rpos = self.heads
+                .binary_search_by(|a| Bucket::<T>::head_cmp(&a.1, &end.1, &mut sort_fn))
+                .unwrap();
+
+            let mut frpos = rpos;
+            let mut found = false;
+            while frpos < self.heads.len() && Bucket::<T>::head_cmp(&self.heads[frpos].1, &end.1, &mut sort_fn) == Ordering::Equal {
+                if self.heads[frpos].0 == i {
+                    found = true;
+                    break;
+                }
+                frpos += 1;
+            }
+
+            if !found && frpos != 0 {
+                frpos = rpos - 1;
+                while Bucket::<T>::head_cmp(&self.heads[frpos].1, &end.1, &mut sort_fn) == Ordering::Equal {
+                    if self.heads[frpos].0 == i {
+                        found = true;
+                        break;
+                    }
+                    if frpos == 0 {
+                        break;
+                    }
+                    frpos -= 1;
+                }
+            }
+
+            assert!(found);
+            self.heads.remove(frpos);
+
+            let new_elem = (i, Some(new_elem.clone()));
+            let pos = self.heads
+                .binary_search_by(|a| Bucket::<T>::head_cmp(&a.1, &new_elem.1, &mut sort_fn))
+                .unwrap_or_else(|o| o);
+            assert!(pos >= frpos);
+            self.heads.insert(pos, new_elem);
+        }
+        self.buckets[i].insert(pos, new_elem);
+    }
+
+    #[inline]
+    fn max<S>(&self, mut sort_fn: S) -> Option<T>
+    where
+        S: FnMut(&T, &T) -> Ordering,
+    {
+        self.buckets
+            .iter()
+            .max_by(|a, b| Bucket::<T>::head_cmp(&a.first().cloned(), &b.first().cloned(), &mut sort_fn))
+            .unwrap()
+            .first()
+            .map(|(_, a)| a)
+            .cloned()
+    }
+
+    #[inline]
+    fn min(&self) -> Option<T> {
+        self.heads
+            .last()
+            .unwrap()
+            .1
+            .clone()
+            .map(|(_, a)| a)
+    }
+
+    #[inline]
+    fn modify<M, S, B>(mut self, modify_fn: M, sort_fn: S, bucket_fn: B) -> Self
+    where
+        M: FnMut(&mut T),
+        S: FnMut(&T, &T) -> Ordering,
+        B: FnMut(&T, usize) -> usize,
+    {
+        self.items.iter_mut().for_each(modify_fn);
+        Bucket::new(self.items, sort_fn, bucket_fn, self.buckets.len())
     }
 }
